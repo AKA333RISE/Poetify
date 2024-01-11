cac62d1fa82c9f76f501ef0a8af9ebf4eb0e0948
Adithyakrishna Arunkumar <adithyakrishna10000@gmail.com>
2024-01-11 13:46:02 -0800

    Preparing Data
    Added testing and training data using NumPy.

--- main.py
+++ main.py
@@ -6,5 +6,34 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Actvation
 from keras.optimizers import RMSprop
 
-filepath = tf.keras.utils.get_file('Shakesphere_reference.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+# We are going to use Shakesphere poems to train the model!
+filepath = tf.keras.utils.get_file('Shakesphere_reference.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt') #To get the file from the website
 poem_text = open(filepath,'rb').read().decode(encoding='utf-8').lower()
+
+poem_text = poem_text[100000:800000]
+
+characters = sorted(set(poem_text))
+
+get_index = dict((c, i) for i, c in enumerate(characters)) #We create a dictionary with all possible characters and numbers corresponding to them 
+get_char = dict((i, c) for i, c in enumerate(characters)) #For finding characters from numbers
+
+SEQ_LENGTH = 40 #We use 40 characters to predict the next character
+STEP_SIZE = 3   #To find next sequence
+
+sentences = [] #Array where sentence is stored
+next_char = [] #Array where next character is stored
+
+for i in range(0, len(poem_text) - SEQ_LENGTH, STEP_SIZE): #Loop to get sentences and next characters
+    sentences.append(poem_text[i: i + SEQ_LENGTH])
+    next_char.append(poem_text[i + SEQ_LENGTH])
+
+
+x = np.zeros((len(sentences), SEQ_LENGTH,
+              len(characters)), dtype=np.bool) #For training data 
+y = np.zeros((len(sentences),
+              len(characters)), dtype=np.bool) #For test data
+
+for i, st in enumerate(sentences): #For loop to set 1 in numpy array if character occurs in that position
+    for t, char in enumerate(st):
+        x[i, t, get_index[char]] = 1
+    y[i, get_index[next_char[i]]] = 1
